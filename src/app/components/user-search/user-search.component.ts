e32ee889c6155d5650b6474bdfeba17679dbfa74
ViniciusBCavalcante <vinicius.cavalcante05@gmail.com>
2022-09-10 17:04:13 -0300

    Add user history and input/filter buttons

--- src/app/components/user-search/user-search.component.ts
+++ src/app/components/user-search/user-search.component.ts
@@ -3,13 +3,53 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-search',
   templateUrl: './user-search.component.html',
-  styleUrls: ['./user-search.component.scss']
+  styleUrls: ['./user-search.component.scss'],
 })
-export class UserSearchComponent implements OnInit {
+export class UserSearchComponent {
+  value3: string;
+  filtrado: boolean = false;
+  username: any;
+  user: any;
+  usernames: any;
+  username_list: any = [];
+  username_filtrado: any = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleFiltro() {
+    this.filtrado = true;
+    this.getUserHistorico();
   }
 
+  getUserHistorico() {
+    let usernames: any = [];
+    this.username_list.push(this.username);
+    console.log(this.username_list);
+
+    // Transformação de array em lista de objetos
+    var list = Object.assign({}, this.username_list);
+    console.log(list);
+    usernames = Object.keys(list).map(function (key) {
+      return { id: key, user: list[key] };
+    });
+    console.log(usernames);
+
+    // Remoção dos usernames duplicados
+    var key = ['user'];
+    this.username_filtrado = usernames.filter(
+      (
+        (s) => (o: { [x: string]: any }) =>
+          ((k) => !s.has(k) && s.add(k))(key.map((k) => o[k]).join('|'))
+      )(new Set())
+    );
+    console.log(this.username_filtrado);
+  }
+
+  setUser(user: any) {
+    console.log(user[0]);
+    this.user = user[0];
+    this.getUserHistorico();
+  }
 }
