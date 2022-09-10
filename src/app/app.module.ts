0960f4354600ade8cf39125fdc9764be84b1c6a3
ViniciusBCavalcante <vinicius.cavalcante05@gmail.com>
2022-09-10 09:40:06 -0300

    Add not found component and adjusts at app routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserSearchComponent } from './components/user-search/user-search.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserSearchComponent, NotFoundComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
