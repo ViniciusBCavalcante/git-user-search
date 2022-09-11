a00d3e2e53c5111db9b036014e66ad5fe3de81bd
ViniciusBCavalcante <vinicius.cavalcante05@gmail.com>
2022-09-11 09:19:04 -0300

    Add repo-info component and specific module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RepoInfoModule } from './components/repo-info/repo-info.module';
 import { UserInfoModule } from './components/user-info/user-info.module';
 import { UserSearchModule } from './components/user-search/user-search.module';
 import { NgModule } from '@angular/core';
@@ -6,10 +7,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RepoInfoComponent } from './components/repo-info/repo-info.component';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, UserSearchModule],
+  imports: [BrowserModule, AppRoutingModule, UserSearchModule, RepoInfoModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/repo-info/repo-info.module.ts
@@ -0,0 +1,10 @@
+import { RepoInfoComponent } from './repo-info.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [RepoInfoComponent],
+  imports: [CommonModule],
+  exports: [RepoInfoComponent],
+})
+export class RepoInfoModule {}
